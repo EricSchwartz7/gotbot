e9355588f8525a71efc16bf02131f228a1d84c59
github username <github email address>
2017-01-05 11:19:22 -0500

    refactored dracarys

--- app.rb
+++ app.rb
@@ -20,8 +20,7 @@ post '/gateway' do
 
   case message
     when 'dracarys'
-      fire = ":fire:" * 25
-      fire_dragon = fire + ":dragon_face: \n"
+      fire = ":fire:" * 25 + ":dragon_face:\n"
       respond_message(fire_dragon * 3)
     when 'valar morghulis'
       respond_message 'https://s-media-cache-ak0.pinimg.com/736x/6d/88/bc/6d88bcebfd0b4b4e3ed58bf020248285.jpg'
